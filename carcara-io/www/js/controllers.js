353097cffe51e9ab18aa930d94a76801ef085673
Dayanne Araujo <dayanne.araujo8@gmail.com>
2016-04-09 18:06:09 -0300

    Material design UI

--- carcara-io/www/js/controllers.js
+++ carcara-io/www/js/controllers.js
@@ -1,209 +1,178 @@
-/*
+/* global angular, document, window */
+'use strict';
 
-  DeepBlue Starter Kit - version 1.1
-  Copyright (c) 2015 INMAGIK SRL - www.inmagik.com
-  All rights reserved
+angular.module('starter.controllers', [])
 
-  written by Mauro Bianchi
-  bianchimro@gmail.com
+.controller('AppCtrl', function($scope, $ionicModal, $ionicPopover, $timeout) {
+    // Form data for the login modal
+    $scope.loginData = {};
+    $scope.isExpanded = false;
+    $scope.hasHeaderFabLeft = false;
+    $scope.hasHeaderFabRight = false;
 
-  file: controllers.js
-  description: this file contains all controllers of the DeepBlue app.
+    var navIcons = document.getElementsByClassName('ion-navicon');
+    for (var i = 0; i < navIcons.length; i++) {
+        navIcons.addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+    }
 
-*/
+    ////////////////////////////////////////
+    // Layout Methods
+    ////////////////////////////////////////
 
-
-//controllers are packed into a module
-angular.module('deepBlue.controllers', [])
-
-//top view controller
-.controller('AppCtrl', function($scope, $rootScope, $state) {
-
-  // #SIMPLIFIED-IMPLEMENTATION:
-  // Simplified handling and logout function.
-  // A real app would delegate a service for organizing session data
-  // and auth stuff in a better way.
-  $rootScope.user = {};
-
-  $scope.logout = function(){
-    $rootScope.user = {};
-    $state.go('app.start')
-  };
-
-})
-
-// This controller is bound to the "app.account" view
-.controller('AccountCtrl', function($scope, $rootScope) {
-
-  //readonly property is used to control editability of account form
-  $scope.readonly = true;
-
-  // #SIMPLIFIED-IMPLEMENTATION:
-  // We act on a copy of the root user
-  $scope.accountUser = angular.copy($rootScope.user);
-  var userCopy = {};
-
-  $scope.startEdit = function(){
-    $scope.readonly = false;
-    userCopy = angular.copy($scope.user);
-  };
-
-  $scope.cancelEdit = function(){
-    $scope.readonly = true;
-    $scope.user = userCopy;
-  };
-
-  // #SIMPLIFIED-IMPLEMENTATION:
-  // this function should call a service to update and save
-  // the data of current user.
-  // In this case we'll just set form to readonly and copy data back to $rootScope.
-  $scope.saveEdit = function(){
-    $scope.readonly = true;
-    $rootScope.user = $scope.accountUser;
-  };
-
-})
-
-
-.controller('LoginCtrl', function ($scope, $state, $rootScope) {
-
-  // #SIMPLIFIED-IMPLEMENTATION:
-  // This login function is just an example.
-  // A real one should call a service that checks the auth against some
-  // web service
-
-  $scope.login = function(){
-    //in this case we just set the user in $rootScope
-    $rootScope.user = {
-      email : "mary@ubiqtspaces.com",
-      name : "Mary Ubiquitous",
-      address : "Rue de Galvignac",
-      city : "RonnieLand",
-      zip  : "00007",
-      avatar : 'sampledata/images/avatar.jpg'
+    $scope.hideNavBar = function() {
+        document.getElementsByTagName('ion-nav-bar')[0].style.display = 'none';
     };
-    //finally, we route our app to the 'app.shop' view
-    $state.go('app.shop');
-  };
+
+    $scope.showNavBar = function() {
+        document.getElementsByTagName('ion-nav-bar')[0].style.display = 'block';
+    };
+
+    $scope.noHeader = function() {
+        var content = document.getElementsByTagName('ion-content');
+        for (var i = 0; i < content.length; i++) {
+            if (content[i].classList.contains('has-header')) {
+                content[i].classList.toggle('has-header');
+            }
+        }
+    };
+
+    $scope.setExpanded = function(bool) {
+        $scope.isExpanded = bool;
+    };
+
+    $scope.setHeaderFab = function(location) {
+        var hasHeaderFabLeft = false;
+        var hasHeaderFabRight = false;
+
+        switch (location) {
+            case 'left':
+                hasHeaderFabLeft = true;
+                break;
+            case 'right':
+                hasHeaderFabRight = true;
+                break;
+        }
+
+        $scope.hasHeaderFabLeft = hasHeaderFabLeft;
+        $scope.hasHeaderFabRight = hasHeaderFabRight;
+    };
+
+    $scope.hasHeader = function() {
+        var content = document.getElementsByTagName('ion-content');
+        for (var i = 0; i < content.length; i++) {
+            if (!content[i].classList.contains('has-header')) {
+                content[i].classList.toggle('has-header');
+            }
+        }
+
+    };
+
+    $scope.hideHeader = function() {
+        $scope.hideNavBar();
+        $scope.noHeader();
+    };
+
+    $scope.showHeader = function() {
+        $scope.showNavBar();
+        $scope.hasHeader();
+    };
+
+    $scope.clearFabs = function() {
+        var fabs = document.getElementsByClassName('button-fab');
+        if (fabs.length && fabs.length > 1) {
+            fabs[0].remove();
+        }
+    };
+})
+
+.controller('LoginCtrl', function($scope, $timeout, $stateParams, ionicMaterialInk) {
+    $scope.$parent.clearFabs();
+    $timeout(function() {
+        $scope.$parent.hideHeader();
+    }, 0);
+    ionicMaterialInk.displayEffect();
+})
+
+.controller('FriendsCtrl', function($scope, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion) {
+    // Set Header
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.$parent.setHeaderFab('left');
+
+    // Delay expansion
+    $timeout(function() {
+        $scope.isExpanded = true;
+        $scope.$parent.setExpanded(true);
+    }, 300);
+
+    // Set Motion
+    ionicMaterialMotion.fadeSlideInRight();
+
+    // Set Ink
+    ionicMaterialInk.displayEffect();
+})
+
+.controller('ProfileCtrl', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk) {
+    // Set Header
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.isExpanded = false;
+    $scope.$parent.setExpanded(false);
+    $scope.$parent.setHeaderFab(false);
+
+    // Set Motion
+    $timeout(function() {
+        ionicMaterialMotion.slideUp({
+            selector: '.slide-up'
+        });
+    }, 300);
+
+    $timeout(function() {
+        ionicMaterialMotion.fadeSlideInRight({
+            startVelocity: 3000
+        });
+    }, 700);
+
+    // Set Ink
+    ionicMaterialInk.displayEffect();
+})
+
+.controller('ActivityCtrl', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk) {
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.isExpanded = true;
+    $scope.$parent.setExpanded(true);
+    $scope.$parent.setHeaderFab('right');
+
+    $timeout(function() {
+        ionicMaterialMotion.fadeSlideIn({
+            selector: '.animate-fade-slide-in .item'
+        });
+    }, 200);
+
+    // Activate ink for controller
+    ionicMaterialInk.displayEffect();
+})
+
+.controller('GalleryCtrl', function($scope, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion) {
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.isExpanded = true;
+    $scope.$parent.setExpanded(true);
+    $scope.$parent.setHeaderFab(false);
+
+    // Activate ink for controller
+    ionicMaterialInk.displayEffect();
+
+    ionicMaterialMotion.pushDown({
+        selector: '.push-down'
+    });
+    ionicMaterialMotion.fadeSlideInRight({
+        selector: '.animate-fade-slide-in .item'
+    });
 
 })
 
-
-// Feeds controller.
-.controller('FeedsCtrl', function($scope, BackendService) {
-
-  // #SIMPLIFIED-IMPLEMENTATION:
-  // In this example feeds are loaded from a json file.
-  // (using "getFeeds" method in BackendService, see services.js)
-  // In your application you can use the same approach or load
-  // feeds from a web service.
-
-  $scope.doRefresh = function(){
-      BackendService.getFeeds()
-      .success(function(newItems) {
-        $scope.feeds = newItems;
-      })
-      .finally(function() {
-        // Stop the ion-refresher from spinning
-        $scope.$broadcast('scroll.refreshComplete');
-      });
-  };
-
-  // Triggering the first refresh
-  $scope.doRefresh();
-
-})
-
-// Shop controller.
-.controller('ShopCtrl', function($scope, $ionicActionSheet, BackendService, CartService) {
-
-  // In this example feeds are loaded from a json file.
-  // (using "getProducts" method in BackendService, see services.js)
-  // In your application you can use the same approach or load
-  // products from a web service.
-
-  //using the CartService to load cart from localStorage
-  $scope.cart = CartService.loadCart();
-
-  $scope.doRefresh = function(){
-      BackendService.getProducts()
-      .success(function(newItems) {
-        $scope.products = newItems;
-      })
-      .finally(function() {
-        // Stop the ion-refresher from spinning (not needed in this view)
-        $scope.$broadcast('scroll.refreshComplete');
-      });
-  };
-
-  // private method to add a product to cart
-  var addProductToCart = function(product){
-    $scope.cart.products.push(product);
-    CartService.saveCart($scope.cart);
-  };
-
-  // method to add a product to cart via $ionicActionSheet
-  $scope.addProduct = function(product){
-    $ionicActionSheet.show({
-       buttons: [
-         { text: '<b>Add to cart</b>' }
-       ],
-       titleText: 'Buy ' + product.title,
-       cancelText: 'Cancel',
-       cancel: function() {
-          // add cancel code if needed ..
-       },
-       buttonClicked: function(index) {
-         if(index == 0){
-           addProductToCart(product);
-           return true;
-         }
-         return true;
-       }
-     });
-  };
-
-  //trigger initial refresh of products
-  $scope.doRefresh();
-
-})
-
-// controller for "app.cart" view
-.controller('CartCtrl', function($scope, CartService, $ionicListDelegate) {
-
-  // using the CartService to load cart from localStorage
-  $scope.cart = CartService.loadCart();
-
-  // we assign getTotal method of CartService to $scope to have it available
-  // in our template
-  $scope.getTotal = CartService.getTotal;
-
-  // removes product from cart (making in persistent)
-  $scope.dropProduct = function($index){
-    $scope.cart.products.splice($index, 1);
-    CartService.saveCart($scope.cart);
-    // as this method is triggered in an <ion-option-button>
-    // we close the list after that (not strictly needed)
-    $ionicListDelegate.closeOptionButtons();
-
-  }
-})
-
-.controller('CheckoutCtrl', function($scope, CartService, $state) {
-
-  //using the CartService to load cart from localStorage
-  // $scope.cart = CartService.loadCart();
-  // $scope.getTotal = CartService.getTotal;
-  //
-  // $scope.getTotal = CartService.getTotal;
-
-  // #NOT-IMPLEMENTED: This method is just calling alert()
-  // you should implement this method to connect an ecommerce
-  // after that the cart is reset and user is redirected to shop
-  $scope.checkout = function(){
-    // alert("this implementation is up to you!");
-    // $scope.cart = CartService.resetCart();
-    // $state.go('app.shop')
-  }
-
-})
+;
